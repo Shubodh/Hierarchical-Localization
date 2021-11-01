1f4be6a115ac72de63f99320c5b26f46b7498e0e
Shubodh ADA g08 <p.saishubodh@gmail.com>
2021-11-01 15:32:31 +0530

    top4room_allimagesfromroom_skip10

--- pipeline_InLoc_metric_level_ADA_INLOC-data.py
+++ pipeline_InLoc_metric_level_ADA_INLOC-data.py
@@ -42,7 +42,7 @@ pairs = Path('pairs/inloc/')
 loc_pairs = pairs / 'pairs-query-netvlad40-minustop3rooms.txt'  # top 40 retrieved by NetVLAD
 
 outputs = Path('/data/InLoc_dataset/outputs/inloc/')  # where everything will be saved
-results = outputs / 'InLoc_hloc_superpoint+superglue_netvlad40-minustop3rooms_skip10_dt011121-t0331.txt'  # the result file
+results = outputs / 'InLoc_hloc_superpoint+superglue_netvlad40-minustop3rooms_skip5_dt011121-t0331.txt'  # the result file
 
 
 # list the standard configurations available
@@ -69,7 +69,7 @@ match_path = match_features.main(matcher_conf, loc_pairs, feature_conf['output']
 
 localize_inloc.main(
     dataset, loc_pairs, feature_path, match_path, results,
-    skip_matches=10) #20. 10 is giving error currently, for 1 query, unable to find any matches > 20  # skip database images with too few matches
+    skip_matches=5) #20. 10 is giving error currently, for 1 query, unable to find any matches > 20  # skip database images with too few matches
 
 
 # ## Visualization
