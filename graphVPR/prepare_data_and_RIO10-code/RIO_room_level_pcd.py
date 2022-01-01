8ec6e7576408225c5597aefb1b67c076e15cfb93
shubodhsai <p.saishubodh@gmail.com>
2022-01-01 23:46:43 +0530

    RIO10 room level ongoing

---
+++ graphVPR/prepare_data_and_RIO10-code/RIO_room_level_pcd.py
@@ -0,0 +1,13 @@
+
+from pathlib import Path
+
+
+if __name__=='__main__':
+    
+    input_path = "/home/shubodh/Downloads/data-non-onedrive/RIO10_data/scene01/seq01/seq01_01/"
+
+    frame_id = "frame-000000"
+    rgb_ext = ".color.jpg"
+    pose_ext = ".pose.txt"
+    depth_ext = ".rendered.depth.png"
+    camera_params_file = "camera.yaml"
