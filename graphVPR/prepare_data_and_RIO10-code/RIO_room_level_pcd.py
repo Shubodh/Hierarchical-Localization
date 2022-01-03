ad062304aaf1e72581b74be4d26a53769c3fb6a8
Shubodh Simulation Server <p.saishubodh@gmail.com>
2022-01-03 07:17:03 +0530

    small changes

--- graphVPR/prepare_data_and_RIO10-code/RIO_room_level_pcd.py
+++ graphVPR/prepare_data_and_RIO10-code/RIO_room_level_pcd.py
@@ -4,34 +4,41 @@ from pathlib import Path
 import numpy as np
 import cv2
 import sys
+import yaml
 
-def pcd_from_depth(rgb_path, depth_path):
-    #depth_path = os.path.join('./data/' +img_id + "_depth.png")
-    #rgb_path = os.path.join('./data/' +img_id + "_rgb.png")
+def camera_intrinsics(camera_params):
+        fx, fy, cx, cy  = camera_params['camera_intrinsics']['model']
+        width, height = camera_params['camera_intrinsics']['width'], camera_params['camera_intrinsics']['height']
+        cam_int = o3d.camera.PinholeCameraIntrinsic(width,height,fx,fy,cx,cy)
+        #print(width, height, fx, fy, cx, cy)
+        #print("Camera intrinsics matrix:")
+        #print(cam_int.intrinsic_matrix)
+        return cam_int
+
+def pcd_from_depth(camera_params, rgb_path, depth_path):
     color_raw = o3d.io.read_image(rgb_path)
-
-    #discard the 4th channel of RGB image
-    clr_arr = np.asarray(color_raw)
-    clr_arr_new = clr_arr[:,:,:3]
-    color_raw_new = o3d.geometry.Image(clr_arr_new.astype(np.uint8))
-
     depth_raw = o3d.io.read_image(depth_path)
+    #o3d.visualization.draw_geometries([depth_raw])
 
-    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw_new, depth_raw,depth_scale=1000.0,depth_trunc=1000.0,convert_rgb_to_intensity=False)
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw,depth_scale=1000.0,depth_trunc=1000.0,convert_rgb_to_intensity=False)
 
-    cam_int = camera_intrinsics(habitat=True)
-    pcd_query = o3d.geometry.PointCloud.create_from_rgbd_image(
+    cam_int = camera_intrinsics(camera_params)
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
             rgbd_image, cam_int)
+    o3d.visualization.draw_geometries([pcd])
 
+    return pcd
 
 if __name__=='__main__':
     
     input_path = "/home/shubodh/hdd1/Shubodh/Downloads/data-non-onedrive/RIO10_data/scene01/seq01/seq01_01/"
     #input_path = "/home/shubodh/Downloads/data-non-onedrive/RIO10_data/scene01/seq01/seq01_01/"
 
-    frame_id = "frame-000000"
+    frame_id = "frame-000016"
     rgb_ext = ".color.jpg"
     pose_ext = ".pose.txt"
     depth_ext = ".rendered.depth.png"
-    camera_params_file = "camera.yaml"
-    pcd_from_depth(rgb_path = input_path + frame_id + rgb_ext, depth_path=input_path+frame_id+depth_ext)
+    camera_params = yaml.load(open(input_path + "camera.yaml"), Loader=yaml.FullLoader)
+
+    pcd = pcd_from_depth(camera_params,
+                rgb_path = input_path + frame_id + rgb_ext, depth_path=input_path+frame_id+depth_ext)
