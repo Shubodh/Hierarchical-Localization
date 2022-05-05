d01f9a8e9477c0194c7e00ce0d130f1ea7a97deb
Shubodh ADA g08 <p.saishubodh@gmail.com>
2022-05-05 12:21:22 +0530

    tiny changes

---
+++ hloc/utils/bash_camera_projection_helper_3D_feature_generation.sh
@@ -0,0 +1,10 @@
+#echo "scene 01"
+#python3 camera_projection_helper.py --db_dir /data/InLoc_like_RIO10/sampling10/scene01_JUST/ --save_dir /data/InLoc_dataset/outputs/rio/ICCV_TEST --scene_id 01
+echo "scene 03"
+python3 camera_projection_helper.py --db_dir /data/InLoc_like_RIO10/sampling10/scene03_JUST/ --save_dir /data/InLoc_dataset/outputs/rio/ICCV_TEST --scene_id 03
+echo "scene 05"
+python3 camera_projection_helper.py --db_dir /data/InLoc_like_RIO10/sampling10/scene05_JUST/ --save_dir /data/InLoc_dataset/outputs/rio/ICCV_TEST --scene_id 05
+echo "scene 07"
+python3 camera_projection_helper.py --db_dir /data/InLoc_like_RIO10/sampling10/scene07_JUST/ --save_dir /data/InLoc_dataset/outputs/rio/ICCV_TEST --scene_id 07
+echo "scene 09"
+python3 camera_projection_helper.py --db_dir /data/InLoc_like_RIO10/sampling10/scene09_JUST/ --save_dir /data/InLoc_dataset/outputs/rio/ICCV_TEST --scene_id 09
